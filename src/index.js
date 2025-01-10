f6ebc523c2be3e54092da40dbc1c1d120c406a7f
tronghia <trongnghiakiz@gmail.com>
2025-01-10 21:18:53 +0700

    setup

--- src/index.js
+++ src/index.js
@@ -28,13 +28,32 @@
 
 
 
-
+const path = require('path')
 const express = require('express')
-const app = express()
-const port = 3000
+const morgan = require('morgan')
+const hbs = require('express-handlebars')
 
-app.get('/', function (req, res) {
-  res.send('abcABC')
+const app = express()
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', hbs.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(3000)
